e5d4e487ac8225abdab857bd79497d9924e3cd71
Eвгений <mocard778@gmail.com>
2023-05-06 07:49:52 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+    }
 
+    private static void site(String login, String password, String confirmPassword) {
+        if (login.length() > 20) {
+            throw new WrongLoginException();
+        }
+        if (password.length() <= 20 || !password.equals(confirmPassword)) {
+            throw new WrongPasswordException();
+        }
     }
 }
